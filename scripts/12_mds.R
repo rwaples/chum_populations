90371bbfc7233b615803a8035c18621a4e34677c
Ryan Waples <ryan.waples@gmail.com>
2014-10-24 17:15:15 -0700

    added Makefile for plink analysis and R plotting

--- scripts/12_mds.R
+++ scripts/12_mds.R
@@ -1,11 +1,29 @@
+library(stringr)
 args <- commandArgs(TRUE)
 
-distance_matrix_file = args [1]
-k = args [2]
+POPINFO_file = args[1]
+ind_ID_file = args[2]
+distance_matrix_file = args[3]
+k = args[4]
+file_out = args[5]
+
+#popinfo = read.table("POPINFO.txt", sep = "\t", header = TRUE)
+#ind_IDs = read.table("./data/batch_10/batch_10.mdist.id", sep = "\t", header = FALSE)
+
+popinfo = read.table(POPINFO_file, sep = "\t", header = TRUE)
+ind_IDs = read.table(ind_ID_file, sep = "\t", header = FALSE)
+
+names(ind_IDs) <- c('POPNUM', 'IND')
+ind_IDs$SILLI <- str_split_fixed(ind_IDs$IND, '_', 2)[,1]
+ind_IDs_with_POPINFO <- merge(ind_IDs, popinfo)
+
 
 distance_matrix = data.matrix(read.table(distance_matrix_file, header = FALSE))
-
-mds = cmdscale(distance_matrix, k)
-
-write.table()
+mds = data.frame(cmdscale(distance_matrix, k))
+mds$POPNAME <- ind_IDs_with_POPINFO$POPNAME
+mds$INS <- ind_IDs_with_POPINFO$IND
+mds$TIMING <- ind_IDs_with_POPINFO$TIMING
+mds$REGION <- ind_IDs_with_POPINFO$REGION
+  
+write.table(mds, file = file_out, sep = "\t", quote = FALSE, row.names = FALSE)
 
---
+++ scripts/13_plot_mds.R
@@ -0,0 +1,66 @@
+library(ggplot2)
+library(RColorBrewer)
+args <- commandArgs(TRUE)
+
+mds_file = args[1]
+mds = read.table(mds_file, sep = "\t", header = TRUE)
+mds$TIMING <- factor(mds$TIMING, levels = c('Summer', "Fall", "Winter", "UNK"))
+#mds = read.table("./data/batch_10/batch_10.dist_mds", sep = "\t", header = TRUE)
+
+my_cols <- c("#000000", brewer.pal(9, "Set1"))
+
+plot1v2 <- ggplot(data = mds, aes(x = X1, y = X2)) +
+  geom_hline(yintercept = 0, colour = "gray65") +
+  geom_vline(xintercept = 0, colour = "gray65") +
+  geom_point(size = 8, alpha = .8, mapping = aes(color = factor(POPNAME), shape = factor(TIMING))) +
+  geom_point(size = 3, alpha = .8, mapping = aes(shape = factor(TIMING))) +
+  theme_bw() + coord_equal() + scale_color_manual(values = my_cols) +
+  ggtitle("Puget Sound Chum Salmon Populations")
+ggsave(filename = "./plots/1v2.pdf", plot1v2, width = 12, height = 12)
+
+plot1v2region <- ggplot(data = mds, aes(x = X1, y = X2)) +
+  geom_hline(yintercept = 0, colour = "gray65") +
+  geom_vline(xintercept = 0, colour = "gray65") +
+  geom_point(size = 8, alpha = .8, mapping = aes(color = factor(REGION), shape = factor(TIMING))) +
+  geom_point(size = 3, alpha = .8, mapping = aes(shape = factor(TIMING))) +
+  #  geom_segment(data= batch_10_weights, mapping = aes(x = 0, y= 0, xend = Dim1/3, yend = Dim2/3)) +
+  theme_bw() + coord_equal() + scale_color_brewer(type = 'qual', palette="Set1") +
+  ggtitle("Puget Sound Chum Salmon Populations")
+ggsave(filename = "./plots/1v2region.pdf", plot1v2region, width = 12, height = 12)
+
+plot1v3 <-ggplot(data = mds, aes(x = X1, y = X3)) +
+  geom_hline(yintercept = 0, colour = "gray65") +
+  geom_vline(xintercept = 0, colour = "gray65") +
+  geom_point(size = 8, alpha = .8, mapping = aes(color = factor(POPNAME), shape = factor(TIMING))) +
+  geom_point(size = 3, alpha = .8, mapping = aes(shape = factor(TIMING))) +
+  theme_bw() + coord_equal() + scale_color_manual(values = my_cols) +
+  ggtitle("Puget Sound Chum Salmon Populations")
+ggsave(filename = "./plots/1v3.pdf", plot1v3, width = 12, height = 12)
+
+plot1v3region <-ggplot(data = mds, aes(x = X1, y = X3)) +
+  geom_hline(yintercept = 0, colour = "gray65") +
+  geom_vline(xintercept = 0, colour = "gray65") +
+  geom_point(size = 8, alpha = .8, mapping = aes(color = factor(REGION), shape = factor(TIMING))) +
+  geom_point(size = 3, alpha = .8, mapping = aes(shape = factor(TIMING))) +
+  #  geom_segment(data= batch_10_weights, mapping = aes(x = 0, y= 0, xend = Dim1/3, yend = Dim3/3)) +
+  theme_bw() + coord_equal() +  scale_color_brewer(type = 'qual', palette="Set1") +
+  ggtitle("Puget Sound Chum Salmon Populations")
+ggsave(filename = "./plots/1v3region.pdf", plot1v3region, width = 12, height = 12)
+
+plot2v3 <-ggplot(data = mds, aes(x = X2, y = X3)) +
+  geom_hline(yintercept = 0, colour = "gray65") +
+  geom_vline(xintercept = 0, colour = "gray65") +
+  geom_point(size = 8, alpha = .8, mapping = aes(color = factor(POPNAME), shape = factor(TIMING))) +
+  geom_point(size = 3, alpha = .8, mapping = aes(shape = factor(TIMING))) +
+  theme_bw() + coord_equal() + scale_color_manual(values = my_cols) +
+  ggtitle("Puget Sound Chum Salmon Populations")
+ggsave(filename = "./plots/2v3.pdf", plot2v3, width = 12, height = 12)
+
+plot2v3region <-ggplot(data = mds, aes(x = X2, y = X3)) +
+  geom_hline(yintercept = 0, colour = "gray65") +
+  geom_vline(xintercept = 0, colour = "gray65") +
+  geom_point(size = 8, alpha = .8, mapping = aes(color = factor(REGION), shape = factor(TIMING))) +
+  geom_point(size = 3, alpha = .8, mapping = aes(shape = factor(TIMING))) +
+  theme_bw() + coord_equal() +  scale_color_brewer(type = 'qual', palette="Set1") +
+  ggtitle("Puget Sound Chum Salmon Populations")
+ggsave(filename = "./plots/2v3region.pdf", plot2v3region, width = 12, height = 12)
