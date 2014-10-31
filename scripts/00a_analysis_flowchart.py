416ea38bc40f0b7f6c5abd59993dd9f23fc3147b
Ryan Waples <ryan.waples@gmail.com>
2014-10-30 17:00:06 -0700

    added flowcharts

---
+++ scripts/00a_analysis_flowchart.py
@@ -0,0 +1,12 @@
+import pydot
+import pyparsing
+
+myDOT = pydot.graph_from_dot_file("C:/Users/IPSEG/Desktop/Waples/chum_populations/analysis_flowchart.gv")
+
+myDOT[0].write_png("C:/Users/IPSEG/Desktop/Waples/chum_populations/plots/mapping_flowchart.png")
+
+myDOT[1].write_png("C:/Users/IPSEG/Desktop/Waples/chum_populations/plots/population_analysis_flowchart.png")
+
+myDOT[2].write_png("C:/Users/IPSEG/Desktop/Waples/chum_populations/plots/chum_populations.png")
+
+myDOT.write_png("C:/Users/IPSEG/Desktop/Waples/chum_populations/plots/test.png")
