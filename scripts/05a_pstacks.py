bc5f17c684d6a538ad647cce7f4e3d313fcdf8bf
Ryan Waples <ryan.waples@gmail.com>
2015-02-09 10:07:24 -0800

    inital pstacks

---
+++ scripts/05a_pstacks.py
@@ -0,0 +1,24 @@
+# pstacks
+import os
+import os.path
+import glob
+
+
+bam_dir = "/media/Shared/Data/chum/populations/aln/curated/bowtie2"
+
+
+
+pstacks_cmd = "pstacks -f {} -o /media/Shared/Data/chum/populations/pstacks -i {} --model_type bounded --bound_high 0.05 --alpha 0.1  -m 1 -p 6 -t bam"
+
+
+
+ind_bams = ([os.path.basename(xx)[:-6] for xx in glob.glob(os.path.join(bam_dir, "*.bam"))])
+
+for count, xx in enumerate(individuals):
+    ind_id = count + 1
+    ind_filename = os.path.join(bam_dir, "{}.bam".format(xx))
+    print(pstacks_cmd.format(ind_filename, ind_id))
+
+
+
+
