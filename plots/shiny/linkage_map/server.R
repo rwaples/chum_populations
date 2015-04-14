b7ff8d7a916563b00f1d93e02a3514522bbe8662
Ryan Waples <ryan.waples@gmail.com>
2015-04-13 18:17:32 -0700

    updated shiny plotting

--- plots/shiny/linkage_map/server.R
+++ plots/shiny/linkage_map/server.R
@@ -3,23 +3,30 @@ library(ggplot2)
 library(RColorBrewer)
 
 #load base data
-linkage_map = read.table('/home/ipseg/Desktop/waples/chum_populations/results/batch_4/pop_analysis/map_pop_fst.txt', 
+map_fst = read.table('map_pop_bayescan.txt', 
                  header = TRUE,sep = "\t")
 
 shinyServer(function(input, output) {
+  
   # reactive data (not needed here)
   mydata.reac <- reactive({
-    return(linkage_map)})
+    map_fst_flagged = map_fst
+    fst_to_plot = input$plotfst
+    map_fst_flagged$plotfst = map_fst_flagged[[fst_to_plot]]
+    map_fst_flagged$outlier = map_fst_flagged$qval < input$fdr
+    return(map_fst_flagged)})
   
   # output
   output$plot1 <- renderPlot({
     #ggplot
     mydata <- mydata.reac()
-    p = ggplot(mydata, aes(x=cM, y=FST)) + 
+    p = ggplot(mydata, aes(x=cM, y=plotfst)) + 
         facet_grid(LEP_LG ~ .) +
-        stat_smooth(fill="blue", colour="darkblue", size=2, alpha = 0.1, method = 'loess', span=input$span)+
-        geom_point(alpha = .5) +
-        theme_bw()
+        geom_point(aes(color=outlier), alpha = .7, size = 3) +
+        scale_color_manual(values=c("darkgrey","red")) +
+        stat_smooth(fill="blue", colour="darkblue", size=2, alpha = 0.1, 
+                    method = 'loess', span=input$span) +
+        theme_bw() + theme(strip.text = element_text(size=25))
     print(p)
     
   },height = 20000, width = 1000)
--- plots/shiny/linkage_map/ui.R
+++ plots/shiny/linkage_map/ui.R
@@ -4,11 +4,25 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Kernel-smoothed Fst"),
+  titlePanel("Kernel-smoothed Fst along linkage groups"),
   
   # Sidebar with a slider input for the number of bins
+  
   sidebarPanel(
-    numericInput("span", "alpha (smoothing parameter):", .2, min=.01, max =2.01, step=.01)
+
+    selectInput("plotfst", label = "Fst measure:", 
+                list("PLINK" = 'Fst_plink', "BayeScan" = 'Fst_bayescan'), selected = 'Fst_plink'),
+    selectInput("fdr", label = "False discovery rate:", 
+                choices = c(0.05, 0.01, 0.005, 0.001, .0001), selected = 0.01),
+    numericInput("span", label="alpha (kernel smoothing parameter):", .5, min=.01, max =2.01, step=.01), 
+    h2('-----------------------------'), 
+    h3('Loci are plotting along linkage groups, cM on the x-axis'),
+    h3('Fst on the y-axis'),
+    h3('grey dots are non-outlier loci'),
+    h3('red dots are outlier loci at the selected FDR'), 
+    h3('blue lines trace the kernel-smoothed Fst'),
+    h3('shaded areas show the 95% CI')
+    
   ),
   # Show a plot of the generated distribution
   mainPanel(
