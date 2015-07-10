00343b5a92cbc9a18d8c5b3787a6e7f80e53d3c3
Ryan Waples <ryan.waples@gmail.com>
2015-07-10 15:22:35 -0700

    separate SMARTPCA plots into own notebook, procrustes analysis

--- scripts/procrustes.R
+++ scripts/procrustes.R
@@ -2,24 +2,30 @@ library(CCA)
 library(vegan)
 
 
-rel_mat_1 = read.table('/home/ipseg/Desktop/waples/chum_populations/results/batch_4/EIGENSOFT/paralogs.dom.relmatrix', 
-    sep = "", header = FALSE)
-rel_mat_2 = read.table('/home/ipseg/Desktop/waples/chum_populations/results/batch_4/EIGENSOFT/on_map.codom.subsample.relmatrix', sep = "", header = FALSE)
 
-evec_1 = read.table('/home/ipseg/Desktop/waples/chum_populations/results/batch_4/EIGENSOFT/on_map.codom.subsample.evec', 
+
+
+#rel_mat_1 = read.table('/home/ipseg/Desktop/waples/chum_populations/results/batch_4/EIGENSOFT/paralogs.dom.relmatrix', sep = "", header = FALSE)
+#rel_mat_2 = read.table('/home/ipseg/Desktop/waples/chum_populations/results/batch_4/EIGENSOFT/on_map.codom.subsample.relmatrix', sep = "", header = FALSE)
+
+evec_1 = read.table('/home/ipseg/Desktop/waples/chum_populations/results/batch_4/EIGENSOFT/complete.codom.evec', 
                        sep = "", header = FALSE, skip =1)
-evec_2 = read.table('/home/ipseg/Desktop/waples/chum_populations/results/batch_4/EIGENSOFT/on_map.codom.evec', 
+evec_2 = read.table('/home/ipseg/Desktop/waples/chum_populations/results/batch_4/EIGENSOFT/complete.dom.evec', 
                     sep = "", header = FALSE, skip =1)
+
+evec_3 = read.table('/home/ipseg/Desktop/waples/chum_populations/results/batch_4/EIGENSOFT/paralogs.dom.evec', 
+										sep = "", header = FALSE, skip =1)
 
 # CCA
 my_cca = cc(evec_1[2:5], evec_2[2:6])
 
 
 # procrustes
-proc1 = procrustes( evec_2[2:5], evec_1[2:5], symmetric=TRUE)
+proc1 = procrustes( evec_2[2:7], evec_1[2:7], symmetric=TRUE)
+plot(proc1, kind = 2)
 plot(proc1, kind = 1, choices=c(1,2))
 proc1$scale
-hist(residuals(proc1))
+(residuals(proc1))
 proc1
 
 
@@ -28,9 +34,20 @@ plot(proc2)
 proc2$scale
 proc2
 
+proc3 = procrustes(evec_3[2:11], evec_1[2:11], symmetric=TRUE)
+plot(proc3)
+proc3$scale
+proc3
+
+
+
+
 protest(evec_2[2:6], evec_1[2:6], perm = 100000)
 
-
+protest(evec_3[2:11], evec_1[2:11], perm = 100000)
+					 
+					 
+					 
 my_cca = cc(evec_1[2:6], evec_2[2:6])
 
 my_cca$cor
